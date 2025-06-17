546fbc4db1363160dee555bf47cbfae8c23e36ea
Mads Bruun Jensen <31339803+Ow1Dev@users.noreply.github.com>
2025-06-17 20:36:13 +0200

    feat: init

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Configuration for FuncWoo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        name = "FuncWoo";
+        vendorHash = "sha256-int+VOY11p/Vg/ZVKf2I37AejHvH0EUZJoW+U8EFVbQ=";
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            inputsFrom = [self'.packages.default];
+            buildInputs = with pkgs; [
+              go_1_24
+              just
+            ];
+          };
+        };
+
+        packages = {
+          default = pkgs.buildGoModule {
+            inherit name vendorHash;
+            src = ./.;
+            subPackages = ["cmd"];
+            postBuild = ''
+              mv $GOPATH/bin/cmd $GOPATH/bin/funcwoo
+            '';
+          };
+        };
+      };
+    };
+}
