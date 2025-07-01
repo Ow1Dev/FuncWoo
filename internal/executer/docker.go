f2492790ab0821943ffe223e2c39314e5a32ae7c
Mads Bruun Jensen <31339803+Ow1Dev@users.noreply.github.com>
2025-07-01 19:48:23 +0200

    feat: move the request sending to executer instead of the docker implumentation

--- internal/executer/docker.go
+++ internal/executer/docker.go
@@ -3,46 +3,15 @@ package executer
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/mount"
 	"github.com/docker/docker/client"
 	"github.com/docker/go-connections/nat"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-
-	pb "github.com/Ow1Dev/FuncWoo/pkgs/api/server"
 )
 
 type DockerContainer struct {
 	cli *client.Client
-}
-
-func (d *DockerContainer) execute(key string, body string, ctx context.Context) (string, error) {
-	conn, err := grpc.NewClient("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		return "", fmt.Errorf("failed to connect to gRPC server: %w", err)
-	}
-
-	defer conn.Close()
-
-	client := pb.NewFunctionRunnerServiceClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	fmt.Println("executing command in Docker container for key:", key)
-	fmt.Println("Request body:", body)
-	r, err := client.Invoke(ctx, &pb.InvokeRequest{
-		Payload: body,
-	})
-	if err != nil {
-		return "", fmt.Errorf("failed to execute command in Docker container: %w", err)
-	}
-
-	// Implement the logic to execute the command in the Docker container
-	return r.Output, nil
 }
 
 func (d *DockerContainer) exist(key string, ctx context.Context) bool {
--- internal/executer/executer.go
+++ internal/executer/executer.go
@@ -1,9 +1,17 @@
 package executer
 
-import "context"
+import (
+	"context"
+
+	"time"
+	"fmt"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "github.com/Ow1Dev/FuncWoo/pkgs/api/server"
+)
 
 type Container interface {
-	execute (key string, body string, ctx context.Context) (string, error)
 	exist (key string, ctx context.Context) bool
 	start (key string, ctx context.Context) error
 }
@@ -28,10 +36,37 @@ func(e *Executer) Execeute(key string, body string, ctx context.Context) (string
 		}
 	}
 
-	rsp, err := e.container.execute(key, body, ctx)
+	rsp, err := handleRequest(key, body, ctx)
 	if err != nil {
 		return "", err
 	}
 
 	return rsp, nil
+}
+
+
+func handleRequest(key string, body string, ctx context.Context) (string, error) {
+	conn, err := grpc.NewClient("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return "", fmt.Errorf("failed to connect to gRPC server: %w", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewFunctionRunnerServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	fmt.Println("executing command in Docker container for key:", key)
+	fmt.Println("Request body:", body)
+	r, err := client.Invoke(ctx, &pb.InvokeRequest{
+		Payload: body,
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to execute command in Docker container: %w", err)
+	}
+
+	// Implement the logic to execute the command in the Docker container
+	return r.Output, nil
 }
